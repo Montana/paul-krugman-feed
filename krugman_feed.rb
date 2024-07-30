45be6907074cc821a869f9f5308063e3f6502208
Michael Mendy <montana@linux.com>
2024-07-30 16:24:17 -0700

    updated

--- krugman_feed.rb
+++ krugman_feed.rb
@@ -2,12 +2,12 @@
 
 require 'rss'
 require 'time'
+require 'logger'
 
-# krugman data
 class RSSFeedCreator
-  def self.create(articles, author, title, about, link)
+  def self.create(articles, channel_info)
     RSS::Maker.make('2.0') do |maker|
-      set_channel_info(maker.channel, author, title, about, link)
+      set_channel_info(maker.channel, channel_info)
       add_items(maker.items, articles)
     end
   end
@@ -15,13 +15,15 @@ class RSSFeedCreator
   class << self
     private
 
-    def set_channel_info(channel, author, title, about, link)
-      channel.author = author
+    def set_channel_info(channel, info)
+      channel.author = info[:author]
       channel.updated = Time.now.to_s
-      channel.about = about
-      channel.title = title
-      channel.link = link
-      channel.description = about
+      channel.about = info[:about]
+      channel.title = info[:title]
+      channel.link = info[:link]
+      channel.description = info[:description] || info[:about]
+      channel.language = info[:language] || 'en-us'
+      channel.ttl = info[:ttl] || 60
     end
 
     def add_items(items, articles)
@@ -31,9 +33,47 @@ class RSSFeedCreator
           item.title = article[:title]
           item.updated = article[:pub_date].iso8601
           item.description = article[:description]
+          item.author = article[:author] if article[:author]
+          item.category = article[:category] if article[:category]
         end
       end
     end
+  end
+end
+
+class RSSFeedWriter
+  def self.write(rss, filename, logger)
+    File.write(filename, rss.to_s)
+    logger.info("RSS feed created successfully: #{filename}")
+  rescue StandardError => e
+    logger.error("Failed to write RSS feed: #{e.message}")
+    raise
+  end
+end
+
+class RSSFeedManager
+  def initialize(logger = Logger.new($stdout))
+    @logger = logger
+  end
+
+  def generate_and_write_feed(articles, channel_info, output_file)
+    validate_input(articles, channel_info)
+    rss = RSSFeedCreator.create(articles, channel_info)
+    RSSFeedWriter.write(rss, output_file, @logger)
+  rescue StandardError => e
+    @logger.error("An error occurred: #{e.message}")
+    @logger.error(e.backtrace.join("\n"))
+  end
+
+  private
+
+  def validate_input(articles, channel_info)
+    raise ArgumentError, "Articles cannot be empty" if articles.empty?
+    raise ArgumentError, "Channel info is missing required fields" unless valid_channel_info?(channel_info)
+  end
+
+  def valid_channel_info?(info)
+    [:author, :title, :about, :link].all? { |key| info[key] && !info[key].to_s.strip.empty? }
   end
 end
 
@@ -42,28 +82,25 @@ articles = [
     title: 'The Economic Consequences of Mr. Trump',
     link: 'https://www.nytimes.com/2020/01/01/opinion/trump-economy.html',
     description: 'Paul Krugman discusses the economic policies of Donald Trump.',
-    pub_date: Time.parse('2020-01-01')
+    pub_date: Time.parse('2020-01-01'),
+    category: 'Economics'
   },
   {
     title: 'The Rise and Fall of Bitcoin',
     link: 'https://www.nytimes.com/2021/02/15/opinion/bitcoin.html',
     description: 'Paul Krugman analyzes the volatility of Bitcoin.',
-    pub_date: Time.parse('2021-02-15')
+    pub_date: Time.parse('2021-02-15'),
+    category: 'Cryptocurrency'
   }
 ]
 
-begin
-  rss = RSSFeedCreator.create(
-    articles,
-    'Paul Krugman',
-    'Paul Krugman\'s RSS Feed',
-    'Paul Krugman\'s Articles',
-    'https://www.nytimes.com/column/paul-krugman'
-  )
+channel_info = {
+  author: 'Paul Krugman',
+  title: "Paul Krugman's RSS Feed",
+  about: "Paul Krugman's Articles",
+  link: 'https://www.nytimes.com/column/paul-krugman',
+  language: 'en-us',
+  ttl: 120
+}
 
-  output_file = 'paul_krugman_rss.xml'
-  File.write(output_file, rss.to_s)
-  puts "RSS feed created successfully: #{output_file}"
-rescue StandardError => e
-  puts "Failed to create RSS feed: #{e.message}"
-end
+RSSFeedManager.new.generate_and_write_feed(articles, channel_info, 'paul_krugman_rss.xml')
